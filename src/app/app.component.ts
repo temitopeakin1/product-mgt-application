526b68d4b9737a00de53b204f8b1533dfb62ac16
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2023-09-10 11:57:51 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,29 @@
 import { Component } from '@angular/core';
 
-// components metadata (@component is a decorator) 
+// components metadata (@component is a decorator)
 @Component({
   selector: 'pm-root', // defines the component directives name
-  template:`
-    <nav class = 'navbar navbar-expand navbar-light bg-light'>
-      <a class='navbar-brand'>{{pageTitle}}</a>
-      <ul class='nav nav-pills'>
-        <li><a class='nav-link' routerLink='/welcome'>Home</a><li>
-          <li><a class='nav-link' routerLink='/products'>Product List</a><li>
-              <li><a class='nav-link' routerLink='/contact'>Contact</a><li>
-    </ul>
+  template: `
+    <nav class="navbar navbar-expand navbar-light bg-light">
+      <!-- <a class="navbar-brand">{{ pageTitle }}</a> -->
+      <img src="assets/images/plogo.png" width="300"> 
+      <ul class="nav">
+        <li><a class="nav-link" routerLink="/welcome">Home</a></li>
+        <li></li>
+        <li><a class="nav-link" routerLink="/products">Product List</a></li>
+        <li></li>
+        <li><a class="nav-link" routerLink="/contact">Contact</a></li>
+        <li></li>
+        <li><a class="nav-link" routerLink="/login">Login</a></li>
+        <li></li>
+      </ul>
     </nav>
     <div class="container">
-    <router-outlet></router-outlet>
-  </div>
-      `,
-       styleUrls: ['./app.component.css']           
-  })
-
+      <router-outlet></router-outlet>
+    </div>
+  `,
+  styleUrls: ['./app.component.css'],
+})
 export class AppComponent {
-  imageSrc = 'assets/images/tmegha.png'  
-  pageTitle: string = '🏬 Product Management App';
+  imageSrc = 'assets/images/tmegha.png';
 }
-
