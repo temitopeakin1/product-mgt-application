dbee21048c6167d28ec0ae3566fbe2824ba5d76f
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2023-11-11 09:04:03 +0100

    update

--- src/app/home/welcome.component.ts
+++ src/app/home/welcome.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
  
 })
 export class WelcomeComponent {
